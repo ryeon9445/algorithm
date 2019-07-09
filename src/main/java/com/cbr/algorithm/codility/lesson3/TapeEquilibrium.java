90d8b5c55386dbc9be3b8bb21bc8fb5b02568ea5
ryeon9445 <ryeon9445@gmail.com>
2019-07-09 21:50:19 +0900

    TapeEquilibrium

--- src/main/java/com/cbr/algorithm/codility/lesson3/TapeEquilibrium.java
+++ src/main/java/com/cbr/algorithm/codility/lesson3/TapeEquilibrium.java
@@ -6,15 +6,23 @@ public class TapeEquilibrium {
      * O(N * N)
      */
     public static int solution(int[] A) {
+        if (A.length <= 1 || A.length > 100000) {
+            return 0;
+        }
+
         int min = Integer.MAX_VALUE;
         for (int n = 0; n < A.length; n++) {
+            if (A[n] < -1000 || A[n] > 1000) {
+                return 0;
+            }
+
             int leftValue = 0;
             int rightValue = 0;
             for (int p = 0; p < A.length; p++) {
                 if (n >= p) {
-                    leftValue += A[p];
+                    leftValue += Math.abs(A[p]);
                 } else {
-                    rightValue += A[p];
+                    rightValue += Math.abs(A[p]);
                 }
             }
             int calculationValue = Math.abs(leftValue - rightValue);
