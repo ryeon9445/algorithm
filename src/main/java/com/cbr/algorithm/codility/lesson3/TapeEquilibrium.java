dd78169cf5b90b039aaa59c4427ea8ab3e3f3d02
ryeon9445 <ryeon9445@gmail.com>
2019-07-10 20:42:01 +0900

    TapeEquilibrium

--- src/main/java/com/cbr/algorithm/codility/lesson3/TapeEquilibrium.java
+++ src/main/java/com/cbr/algorithm/codility/lesson3/TapeEquilibrium.java
@@ -3,33 +3,26 @@ package com.cbr.algorithm.codility.lesson3;
 public class TapeEquilibrium {
 
     /**
-     * O(N * N)
+     * O(N)
      */
     public static int solution(int[] A) {
-        if (A.length <= 1 || A.length > 100000) {
-            return 0;
+        int minValue = Integer.MAX_VALUE;
+        int leftValue = 0;
+        int rightValue = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            rightValue += A[i];
         }
 
-        int min = Integer.MAX_VALUE;
-        for (int n = 0; n < A.length; n++) {
-            if (A[n] < -1000 || A[n] > 1000) {
-                return 0;
-            }
+        for (int i = 0; i < A.length - 1; i++) {
+            leftValue += A[i];
+            rightValue -= A[i];
 
-            int leftValue = 0;
-            int rightValue = 0;
-            for (int p = 0; p < A.length; p++) {
-                if (n >= p) {
-                    leftValue += Math.abs(A[p]);
-                } else {
-                    rightValue += Math.abs(A[p]);
-                }
-            }
-            int calculationValue = Math.abs(leftValue - rightValue);
-            if (calculationValue < min) {
-                min = calculationValue;
+            int calculatedValue = Math.abs(leftValue - rightValue);
+            if (calculatedValue < minValue) {
+                minValue = calculatedValue;
             }
         }
-        return min;
+        return minValue;
     }
 }
