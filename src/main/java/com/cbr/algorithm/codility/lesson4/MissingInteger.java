99b33cb8b1f6b087d994d13f1695713f21d928ce
ryeon9445 <ryeon9445@gmail.com>
2019-07-17 21:33:20 +0900

    MissingInteger 88%

--- src/main/java/com/cbr/algorithm/codility/lesson4/MissingInteger.java
+++ src/main/java/com/cbr/algorithm/codility/lesson4/MissingInteger.java
@@ -1,24 +1,22 @@
 package com.cbr.algorithm.codility.lesson4;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class MissingInteger {
 
     /**
-     * O(N**2)
+     * O(N) or O(N * log(N))
      */
     public static int solution(int[] A) {
-        int answerValue = 1;
-        while (true) {
-            boolean isSameValue = false;
-            for (int i = 0; i < A.length; i++) {
-                if (A[i] == answerValue) {
-                    isSameValue = true;
-                }
-            }
-            if (isSameValue) {
-                answerValue++;
-            } else {
-                return answerValue;
+        Set<Integer> tempSet = Arrays.stream(A).boxed().collect(Collectors.toSet());
+        for (int i = 1; i < Integer.MAX_VALUE; i++) {
+            if (!tempSet.contains(i)) {
+                return i;
             }
         }
+
+        return -1;
     }
 }
