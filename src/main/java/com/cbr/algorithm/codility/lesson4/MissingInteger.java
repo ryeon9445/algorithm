69bf5e21aba37995340631b912ec59d99f8ad2f6
ryeon9445 <ryeon9445@gmail.com>
2019-07-19 07:57:17 +0900

    MissingInteger 100%

--- src/main/java/com/cbr/algorithm/codility/lesson4/MissingInteger.java
+++ src/main/java/com/cbr/algorithm/codility/lesson4/MissingInteger.java
@@ -1,8 +1,7 @@
 package com.cbr.algorithm.codility.lesson4;
 
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MissingInteger {
 
@@ -10,7 +9,11 @@ public class MissingInteger {
      * O(N) or O(N * log(N))
      */
     public static int solution(int[] A) {
-        Set<Integer> tempSet = Arrays.stream(A).boxed().collect(Collectors.toSet());
+        Set<Integer> tempSet = new HashSet<>();
+        for (int i = 0; i < A.length; i++) {
+            tempSet.add(A[i]);
+        }
+
         for (int i = 1; i < Integer.MAX_VALUE; i++) {
             if (!tempSet.contains(i)) {
                 return i;
