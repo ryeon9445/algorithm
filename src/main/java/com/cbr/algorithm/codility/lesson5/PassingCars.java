bd35853b472d98657bdf945ffbddb3e3c9f78501
ryeon9445 <ryeon9445@gmail.com>
2019-07-29 21:22:19 +0900

    PassingCars 100%

--- src/main/java/com/cbr/algorithm/codility/lesson5/PassingCars.java
+++ src/main/java/com/cbr/algorithm/codility/lesson5/PassingCars.java
@@ -6,27 +6,18 @@ public class PassingCars {
      * O(N)
      */
     public static int solution(int[] A) {
-        int sumValue = 0;
-        int minusValue = 1;
-
-        if (A.length < 1 || A.length > 100000) {
-            return 0;
-        }
+        int result = 0;
+        int sum = 0;
 
         for (int i = 0; i < A.length; i++) {
-            if (A[i] == 0) {
-                sumValue += A.length - (i + 1);
-                if (i != 0 && sumValue > 0) {
-                    sumValue -= minusValue;
-                    minusValue ++;
-                }
-            }
+            if (A[i] == 0)
+                sum++;
+            else
+                result += sum;
         }
 
-        if (sumValue > 1000000000 || sumValue < 0) {
-            return -1;
-        }
+        if (result > 1000000000 || result < 0) return -1;
 
-        return sumValue;
+        return result;
     }
 }
