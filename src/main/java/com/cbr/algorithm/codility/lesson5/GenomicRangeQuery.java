261e5bdd5d149831ea72318d42cc34a97ddae029
ryeon9445 <ryeon9445@gmail.com>
2019-07-31 21:48:03 +0900

    init GenomicRangeQuery

---
+++ src/main/java/com/cbr/algorithm/codility/lesson5/GenomicRangeQuery.java
@@ -0,0 +1,8 @@
+package com.cbr.algorithm.codility.lesson5;
+
+public class GenomicRangeQuery {
+
+    public static int[] solution(String S, int[] P, int[] Q) {
+        return new int[]{2, 4, 1};
+    }
+}
