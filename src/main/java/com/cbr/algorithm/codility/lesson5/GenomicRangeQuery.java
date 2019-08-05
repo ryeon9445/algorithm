20973af2ca32e188c8e10d4ede4d96dc485a2ef8
ryeon9445 <ryeon9445@gmail.com>
2019-08-06 08:35:22 +0900

    GenomicRangeQuery 100%

--- src/main/java/com/cbr/algorithm/codility/lesson5/GenomicRangeQuery.java
+++ src/main/java/com/cbr/algorithm/codility/lesson5/GenomicRangeQuery.java
@@ -3,33 +3,84 @@ package com.cbr.algorithm.codility.lesson5;
 public class GenomicRangeQuery {
 
     public static int[] solution(String S, int[] P, int[] Q) {
-        int[] result = new int[P.length];
+        int[] answer = new int[P.length];
 
-        for (int i = 0; i < P.length; i++) {
-            int minValue = Integer.MAX_VALUE;
-            for (int j = P[i]; j <= Q[i]; j++) {
-                int sValue = 0;
-                switch (S.charAt(j)) {
-                    case 'A':
-                        sValue = 1;
-                        break;
-                    case 'C':
-                        sValue = 2;
-                        break;
-                    case 'G':
-                        sValue = 3;
-                        break;
-                    case 'T':
-                        sValue = 4;
-                        break;
-                }
-                if (minValue > sValue) {
-                    minValue = sValue;
-                }
+        int[] A = new int[S.length()];
+        int[] C = new int[S.length()];
+        int[] G = new int[S.length()];
+
+        // index == 0
+        {
+            char c = S.charAt(0);
+
+            if (c == 'A') {
+                A[0]++;
             }
-            result[i] = minValue;
+            else if (c == 'C') {
+                C[0]++;
+            }
+            else if (c == 'G') {
+                G[0]++;
+            }
         }
 
-        return result;
+        // index > 0
+        for (int i = 1; i < S.length(); i++) {
+            char c = S.charAt(i);
+
+            if (c == 'A') {
+                A[i] = A[i - 1] + 1;
+                C[i] = C[i - 1];
+                G[i] = G[i - 1];
+            }
+            else if (c == 'C') {
+                A[i] = A[i - 1];
+                C[i] = C[i - 1] + 1;
+                G[i] = G[i - 1];
+            }
+            else if (c == 'G') {
+                A[i] = A[i - 1];
+                C[i] = C[i - 1];
+                G[i] = G[i - 1] + 1;
+            }
+            else {
+                A[i] = A[i - 1];
+                C[i] = C[i - 1];
+                G[i] = G[i - 1];
+            }
+        }
+
+        for (int i = 0; i < P.length; i++) {
+            if (P[i] > 0) {
+                if (A[Q[i]] > A[P[i] - 1]) {
+                    answer[i] = 1;
+                }
+                else if (C[Q[i]] > C[P[i] - 1]) {
+                    answer[i] = 2;
+                }
+                else if (G[Q[i]] > G[P[i] - 1]) {
+                    answer[i] = 3;
+                }
+                else {
+                    answer[i] = 4;
+                }
+            }
+            else {
+                if (A[Q[i]] > 0) {
+                    answer[i] = 1;
+                }
+                else if (C[Q[i]] > 0) {
+                    answer[i] = 2;
+                }
+                else if (G[Q[i]] > 0) {
+                    answer[i] = 3;
+                }
+                else {
+                    answer[i] = 4;
+                }
+            }
+        }
+
+        return answer;
     }
 }
